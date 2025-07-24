17b256314ffb9a389748fd087bfc75c2bc74279c
Harkrit-07 <161572715+harkrit07@users.noreply.github.com>
2025-07-24 15:00:33 +0530

    Update model.py

--- Portfolio/portfolio-backend/model.py
+++ Portfolio/portfolio-backend/model.py
@@ -1,11 +1,11 @@
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
 
 
-class Contact(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120),  nullable=False)
-    email = db.Column(db.String(120),  nullable=False)
-    phone = db.Column(db.String(40))
-    message = db.Column(db.Text,         nullable=False)
+# class Contact(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(120),  nullable=False)
+#     email = db.Column(db.String(120),  nullable=False)
+#     phone = db.Column(db.String(40))
+#     message = db.Column(db.Text,         nullable=False)
